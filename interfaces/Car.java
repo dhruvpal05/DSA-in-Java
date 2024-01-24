cb912e531b4ab599907c460fe562600d8f5c043d
dhruvpal05 <dhruv416pal@gmail.com>
2024-01-25 01:24:24 +0530

    interfaces

---
+++ interfaces/Car.java
@@ -0,0 +1,23 @@
+package interfaces;
+
+public class Car implements Brake,Engine{
+
+    @Override
+    public void brake() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'brake'");
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+    
+}
