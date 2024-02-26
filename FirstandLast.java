fcd9f341f63ce8042572043943c5ece70a7cf45c
dhruvpal05 <dhruv416pal@gmail.com>
2024-02-26 14:33:49 +0530

    day2

--- FirstandLast.java
+++ FirstandLast.java
@@ -1,18 +1,34 @@
 public class FirstandLast {
     public static int[] searchRange(int[] nums, int target) {
-        int[] ans;
-        int start = 0;
-        int end = nums.length -1;
-        while (end >= start) {
-            int mid = start + (end - start)/2;
-            if(nums[mid] == target){
-                
-            }
-        }
-
+        int[] ans = {-1,-1};
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+        ans[0] = start;
+        ans[1] = end;
         return ans;
     }
 
+    public static int search(int[] nums, int target, boolean first) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] > target){
+                end = mid - 1;
+            }else if (nums[mid] < target) {
+                start = mid + 1;
+            }else{
+                ans = mid;
+                if(first){
+                    end = mid -1;
+                }else{
+                    start = mid +1;
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         
     }
