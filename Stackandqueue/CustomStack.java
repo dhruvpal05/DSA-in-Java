7a777739326fd7b41d1f0a1411c54ed533b31ed0
dhruvpal05 <dhruv416pal@gmail.com>
2024-06-28 23:20:18 +0530

    stack

--- Stackandqueue/CustomStack.java
+++ Stackandqueue/CustomStack.java
@@ -4,7 +4,7 @@ public class CustomStack {
     protected int[] data;
     private static final int DEFAULT_SIZE = 10;
 
-    int ptr = -1;
+    int ptr = -1    ;
 
     public CustomStack(){
         this(DEFAULT_SIZE);
