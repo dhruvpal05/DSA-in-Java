84e77e955bbb47983645017313b8a4808eb8288a
dhruvpal05 <dhruv416pal@gmail.com>
2024-06-30 01:28:22 +0530

    stack day 3

---
+++ Stackandqueue/MyQueue.java
@@ -0,0 +1,63 @@
+package Stackandqueue;
+
+import java.util.Stack;
+
+public class MyQueue {
+    Stack<Integer> first = new Stack<>();
+    Stack<Integer> second = new Stack<>();
+
+    public MyQueue() {
+        Stack first;
+        Stack second;
+    }
+
+    public void push(int x) {
+        first.push(x);
+    }
+
+    public int pop() {
+        if (!empty()) {
+            while (first.size() > 1) {
+                second.push(first.pop());
+            }
+    
+            int ans = first.pop();
+    
+            while (!second.isEmpty()) {
+                first.push(second.pop());
+            }
+    
+            return ans;
+        }
+        return -1;
+    }
+
+    public int peek() {
+        if (!empty()) {
+            while (first.size() > 1) {
+                second.push(first.pop());
+            }
+    
+            int ans = first.peek();
+    
+            while (!second.isEmpty()) {
+                first.push(second.pop());
+            }
+    
+            return ans;
+        }
+        return -1;
+        
+    }
+
+    public boolean empty() {
+        // if (first.size()>0) {
+        //     return false;
+        // }
+        // return true;
+        if (first.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
