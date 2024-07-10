c870ce35d2069217e5f0351039e2602364eb53bc
dhruvpal05 <dhruv416pal@gmail.com>
2024-07-10 22:10:59 +0530

    sde sheet day5

--- Arrays/AvgWait.java
+++ Arrays/AvgWait.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AvgWait {
-    public double averageWaitingTime(int[][] customers) {
-        
+    public static double averageWaitingTime(int[][] customers) {
+        int start = customers[0][0];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < customers.length; i++) {
+            start = start + customers[i][1];
+            list.add(start-customers[i][0]);
+        }
+        int sum = 0; 
+        for (int num : list) { 
+            sum += num; 
+        } 
+        double average = (double)sum / list.size();
+        return average;
+    }
+    public static void main(String[] args) {
+        int[][] customers = {{1,2},{2,5},{4,3}};
+        averageWaitingTime(customers);
     }
 }
---
+++ Arrays/Logfolder.java
@@ -0,0 +1,18 @@
+public class Logfolder {
+    public int minOperations(String[] logs) {
+        int step = 0;
+        for(String log : logs){
+            if (log.equals("../")) {
+                if (step>0) {
+                    step--;
+                }
+            }
+            else if (log.equals("./")) {
+                continue;
+            }else{
+                step++;
+            }
+        }
+        return step;
+    }
+}
