cc3d965e9570843de3f583e2e697c9515fd4509f
dhruvpal05 <dhruv416pal@gmail.com>
2025-01-17 03:02:07 +0530

    bitmanipulation

---
+++ Bitmanipulation/MinimizeXOR.java
@@ -0,0 +1,7 @@
+package Bitmanipulation;
+
+public class MinimizeXOR {
+    public int minimizeXor(int num1, int num2) {
+        return 0;
+    }
+}
---
+++ String/AtoiStr.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AtoiStr {
+    public static void main(String[] args) {
+        String s = "-42";
+        System.out.println(myAtoi(s));
+    }
+
+    public static int myAtoi(String s) {
+        s = s.trim();
+        if (s.isEmpty())
+            return 0;
+        int i = 0;
+        int n = s.length();
+        boolean isNegative = false;
+        if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+            isNegative = s.charAt(i) == '-';
+            i++;
+        }
+        long result = 0;
+        while (i < n) {
+            char c = s.charAt(i);
+            if (!Character.isDigit(c)) {
+                break;
+            }
+            result = result * 10 + Character.getNumericValue(c);
+            if (result > Integer.MAX_VALUE) {
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            i++;
+        }
+        return isNegative ? (int) -result : (int) result;
+    }
+}
